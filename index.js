9aa4f028c9e99c42f5bb473ac2669005919910aa
Mahbuba-Islam <mahbubaislam7010@gmail.com>
2022-07-30 18:56:23 -0400

    payment-success

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -138,8 +139,21 @@ async function run() {
 
   })
 
+// payment method
+app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
+  const service = req.body;
+  const price = service.price;
+  const amount = price*100;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount : amount,
+    currency: 'usd',
+    payment_method_types:['card']
+  });
+  res.send({clientSecret: paymentIntent.client_secret})
+});
 
 
+    
      app.get('/admin/:email', async(req,res)=>{
     const email=req.params.email
     const user = await userCollection.findOne({email:email})
